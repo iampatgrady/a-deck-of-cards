c2a57251c83b566a7ff9261ebe40cd4afa4cfedb
Pat Grady <pat.grady@adswerve.com>
2019-08-22 00:27:09 -0500

    first commit for a-deck-of-cards.com

---
+++ index.js
@@ -0,0 +1,22 @@
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+//var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+//require("firebase/auth");
+//require("firebase/firestore");
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA2HDvct72Ny1zUHImK7DXT9ECUTzIuFZ0",
+  authDomain: "my-deck-of-cards.firebaseapp.com",
+  databaseURL: "https://my-deck-of-cards.firebaseio.com",
+  projectId: "my-deck-of-cards",
+  storageBucket: "my-deck-of-cards.appspot.com",
+  messagingSenderId: "158657254192",
+  appID: "1:158657254192:web:2b0f950140e306ef",
+};
+
+// Initialize Firebase
+//firebase.initializeApp(firebaseConfig);
